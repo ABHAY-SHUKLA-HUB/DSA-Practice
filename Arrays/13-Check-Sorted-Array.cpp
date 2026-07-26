632e4eaa782d94099ca2a08c3e0ee1f1b34adc13
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-07-26 19:17:47 +0530

    13-Check-Sorted-Array

---
+++ Arrays/13-Check-Sorted-Array.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[100];
+    bool isSorted=true;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<n-1;i++){
+        if(arr[i]>arr[i+1]){
+            isSorted = false;
+            break;
+        }
+    }
+    if(isSorted){
+        cout<<"Sorted";
+    }
+    else{
+        cout<<"Not Sorted";
+    }
+
+}
