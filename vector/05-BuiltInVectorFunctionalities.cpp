9bfc365569c519056ea0c2ca617f4781de7a2e0f
ABHAY-SHUKLA-HUB <shuklaabhayas0@gmail.com>
2026-08-03 20:03:04 +0530

    05

---
+++ vector/05-BuiltInVectorFunctionalities.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+    vector<int> v={4,3,8,2};
+    reverse(v.begin(),v.end());
+    for(int ele : v){
+        cout<<ele<<" ";
+    }
+}
